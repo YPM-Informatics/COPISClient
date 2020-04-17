fcf50624b47b3fc1351a28e19714ec4fc01af83e
Songie Park <songie.park@yale.edu>
2020-04-17 16:53:53 -0400

    Implemented the major part of live view display and live view shut down.

--- evfFrame.py
+++ evfFrame.py
@@ -1,13 +1,2 @@
-import wx
 
-class EvfFrame(wx.Frame):
-    def __init__(self):
-        wx.Frame.__init__(self, None, wx.ID_ANY, "Live View")
-        self.Centre()
 
-    def onDrawImage(self, data):
-        #playImg = wx.Image('img/play.png')
-        self.image = wx.Bitmap().FromBuffer(self.Size.GetWidth(), self.Size.GetHeight(), data)
-
-        dc = wx.PaintDC(self)
-        dc.DrawBitmap(self.image)
