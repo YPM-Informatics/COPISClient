d27c49f8f5b9623400f1b2df30ede082fe9bd200
k3vnchen <k3vnchen@gmail.com>
2020-07-14 00:06:45 -0400

    Enable custom build dimensions and revamped grid creation

--- components/canvas3d.py
+++ components/canvas3d.py
@@ -5,6 +5,8 @@ import math
 import random
 import numpy as np
 import platform as pf
+
+from ctypes import sizeof, c_float, c_void_p, c_uint
 from threading import Lock
 
 import wx
@@ -16,8 +18,8 @@ from components.glhelper import arcball, vector_to_quat, quat_to_matrix4, mul_qu
 from components.path3d import Path3D
 from components.camera3d import Camera3D
 
-class Canvas3D(glcanvas.GLCanvas):
 
+class Canvas3D(glcanvas.GLCanvas):
     class Size():
         def __init__(self, width, height, scale_factor):
             self.__width = width
@@ -48,16 +50,14 @@ class Canvas3D(glcanvas.GLCanvas):
         def scale_factor(self, scale_factor):
             self.__scale_factor = scale_factor
 
-    zoom_min = 0.5
-    zoom_max = 5.0
-    clip_near = 3.0
-    clip_far = 7.0
+    zoom_min = 0.1
+    zoom_max = 7.0
 
     # True: use arcball controls, False: use orbit controls
-    arcball_control = True
+    arcball_control = False
     color_background = (0.941, 0.941, 0.941, 1)
 
-    def __init__(self, parent):
+    def __init__(self, parent, build_dimensions = None):
         display_attrs = glcanvas.GLAttributes()
         display_attrs.MinRGBA(8, 8, 8, 8).DoubleBuffer().Depth(24).EndList()
         super().__init__(parent, display_attrs, -1)
@@ -65,15 +65,21 @@ class Canvas3D(glcanvas.GLCanvas):
         self._gl_initialized = False
         self._dirty = False # dirty flag to track when we need to re-render the canvas
         self._scale_factor = None
-        self._zoom = 1
         self._width = None
         self._height = None
+        self._mouse_pos = None
+        self._zoom = 1
         self._rot_quat = [1, 0, 0, 0]
         self._rot_lock = Lock()
         self._angle_z = 0
         self._angle_x = 0
-        self._mouse_pos = None
         self._points = []
+
+        if build_dimensions:
+            self._build_dimensions = build_dimensions
+        else:
+            self._build_dimensions = [800, 800, 800, 400, 400, 400]
+        self._grid3d = Grid3D(self._build_dimensions, every=100, subdivisions=10)
 
         self._camera3d_list = []
         self._path3d_list = []
@@ -106,15 +112,23 @@ class Canvas3D(glcanvas.GLCanvas):
         glClearColor(*self.color_background)
         glClearDepth(1.0)
 
-        glDepthFunc(GL_LEQUAL)
+        glDepthFunc(GL_LESS)
+
         glEnable(GL_DEPTH_TEST)
         glEnable(GL_CULL_FACE)
         glEnable(GL_BLEND)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
         # set antialiasing
-        glEnable(GL_LINE_SMOOTH)
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        # glEnable(GL_LINE_SMOOTH)
+        # glEnable(GL_POLYGON_SMOOTH)
+
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+        glEnable(GL_COLOR_MATERIAL)
         glEnable(GL_MULTISAMPLE)
+
+        if not self._grid3d.init():
+            return
 
         self._gl_initialized = True
         return True
@@ -140,7 +154,8 @@ class Canvas3D(glcanvas.GLCanvas):
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         self._render_background()
-        self._render_grid()
+
+        self._render_platform()
         self._render_objects()
         self._render_cameras()
         self._render_paths()
@@ -273,50 +288,22 @@ class Canvas3D(glcanvas.GLCanvas):
     def _render_background(self):
         glClearColor(*self.color_background)
 
-    def _render_grid(self):
-        glColor3ub(200, 200, 200)
-        glBegin(GL_LINES)
-        for i in np.arange(-10, 11, 1):
-            i *= 0.1
-            glVertex3f(i, 1, 0)
-            glVertex3f(i, -1, 0)
-            glVertex3f(1, i, 0)
-            glVertex3f(-1, i, 0)
+    def _render_platform(self):
+        if not self._grid3d:
+            return
 
-        glColor3ub(255, 0, 0)
-        glVertex3f(0, 0, 0)
-        glVertex3f(math.sqrt(2), 0, 0)
-        glColor3ub(0, 255, 0)
-        glVertex3f(0, 0, 0)
-        glVertex3f(0, math.sqrt(2), 0)
-        glColor3ub(0, 0, 255)
-        glVertex3f(0, 0, 0)
-        glVertex3f(0, 0, math.sqrt(2))
-        glEnd()
+        self._grid3d.render()
 
     def _render_objects(self):
-        # draw hemispheres
-        glColor3ub(225, 225, 225)
-        draw_circle([0, 0, 0], [1, 1, 0], math.sqrt(2), 128)
-        draw_circle([0, 0, 0], [1, -1, 0], math.sqrt(2), 128)
-        for i in np.arange(0, 180, 45):
-            draw_circle([0, 0, math.sqrt(2) * math.cos(np.deg2rad(i))], [0, 0, 1], math.sqrt(2) * math.sin(np.deg2rad(i)), 128)
+        # draw origin sphere
+        glColor3ub(0, 0, 0)
+        gluSphere(self.quadratic, 5, 32, 32)
 
-        # draw axes circles
-        glColor3ub(160, 160, 160)
-        draw_circle([0, 0, 0], [0, 0, 1], math.sqrt(2), 128)
-        draw_circle([0, 0, 0], [0, 1, 0], math.sqrt(2), 128)
-        draw_circle([0, 0, 0], [1, 0, 0], math.sqrt(2), 128)
-
-        # draw sphere
-        glColor3ub(0, 0, 128)
-        gluSphere(self.quadratic, 0.25, 32, 32)
-
+        glColor3ub(255,0,0)
         for point in self._points:
             glPushMatrix()
-            glColor3ub(255,0,0)
             glTranslate(*point)
-            gluSphere(self.quadratic, 0.25, 5, 5)
+            gluSphere(self.quadratic, 50, 5, 5)
             glPopMatrix()
 
     def _render_cameras(self):
@@ -344,9 +331,9 @@ class Canvas3D(glcanvas.GLCanvas):
 
     def add_camera(self, id=-1):
         """Add new Camera3D."""
-        x = float(random.randrange(-100, 100)) / 100
-        y = float(random.randrange(-100, 100)) / 100
-        z = float(random.randrange(-100, 100)) / 100
+        x = random.random() * self._build_dimensions[0] - self._build_dimensions[3]
+        y = random.random() * self._build_dimensions[1] - self._build_dimensions[4]
+        z = random.random() * self._build_dimensions[2] - self._build_dimensions[5]
         b = random.randrange(0, 360, 5)
         c = random.randrange(0, 360, 5)
 
@@ -386,7 +373,7 @@ class Canvas3D(glcanvas.GLCanvas):
         """Apply modelview matrix according to rotation quat."""
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-        gluLookAt(0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+        gluLookAt(0.0, 0.0, 500.0, 0.0, 0.0, 0.0, 0.0, 100.0, 0.0)
         glMultMatrixd(quat_to_matrix4(self._rot_quat))
 
     def apply_projection(self):
@@ -394,7 +381,11 @@ class Canvas3D(glcanvas.GLCanvas):
         # TODO: add toggle between perspective and orthographic view modes
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        gluPerspective(np.arctan(np.tan(np.deg2rad(50.0)) / self._zoom) * 180 / np.pi, float(self._width) / self._height, self.clip_near, self.clip_far)
+        gluPerspective(
+            np.arctan(np.tan(np.deg2rad(50.0)) / self._zoom) * 180 / np.pi,
+            float(self._width) / self._height,
+            0.1,
+            2000.0)
         glMatrixMode(GL_MODELVIEW)
 
     def get_modelview_matrix(self):
@@ -506,3 +497,100 @@ class Canvas3D(glcanvas.GLCanvas):
             else:
                 self._scale_factor = 1.0
         return self._scale_factor
+
+
+class Grid3D():
+    def __init__(self, build_dimensions, every=100, subdivisions=10):
+        self._build_dimensions = build_dimensions
+        self._every = every
+        self._subdivisions = subdivisions
+        self._initialized = False
+        self._vertices = None
+        self._colors = None
+
+    def init(self):
+        if self._initialized:
+            return True
+
+        self.create()
+
+        self._initialized = True
+        return True
+
+    def render(self):
+        if not self.init():
+            return
+
+        glEnableClientState(GL_VERTEX_ARRAY)
+        glEnableClientState(GL_COLOR_ARRAY)
+
+        glVertexPointer(3, GL_FLOAT, 0, self._vertices)
+        glColorPointer(3, GL_FLOAT, 0, self._colors)
+        glDrawArrays(GL_LINES, 0, self.get_vertices_count())
+
+        glDisableClientState(GL_VERTEX_ARRAY)
+        glDisableClientState(GL_COLOR_ARRAY)
+
+    def create(self):
+        step = self._every / self._subdivisions
+
+        x_neg = self._build_dimensions[3]
+        x_pos = self._build_dimensions[0] - x_neg
+        y_neg = self._build_dimensions[4]
+        y_pos = self._build_dimensions[1] - y_neg
+        z_neg = self._build_dimensions[5]
+        z_pos = self._build_dimensions[2] - z_neg
+
+        # start with axes
+        axes_verts = np.array([
+            -x_neg, 0, 0, x_pos, 0, 0,
+            0, -y_neg, 0, 0, y_pos, 0,
+            0, 0, -z_neg, 0, 0, z_pos])
+        axes_colors = np.array([
+            1.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+            0.0, 1.0, 0.0, 0.0, 1.0, 0.0,
+            0.0, 0.0, 1.0, 0.0, 0.0, 1.0])
+
+        dark   = 0.72
+        light  = 0.90
+        border = 0.40
+        def darken(x):
+            x[self._subdivisions-1::self._subdivisions] = dark
+            x[-1] = border
+            return x
+
+        # gridlines parallel to x axis
+        i = np.append(np.arange(-step, -y_neg, -step), -y_neg)
+        j = np.append(np.arange(step, y_pos, step), y_pos)
+        k = np.concatenate([i, j])
+        x_verts = np.zeros(k.size * 6)
+        x_verts[1::3] = k.repeat(2)
+        x_verts[0::3] = -x_neg
+        x_verts[3::6] = x_pos
+        x_colors = np.concatenate([
+            darken(np.full(i.size, light)),
+            darken(np.full(j.size, light))]).repeat(6)
+
+        # gridlines parallel to y axis
+        i = np.append(np.arange(-step, -x_neg, -step), -x_neg)
+        j = np.append(np.arange(step, x_pos, step), x_pos)
+        k = np.concatenate([i, j])
+        y_verts = np.zeros(k.size * 6)
+        y_verts[0::3] = k.repeat(2)
+        y_verts[1::6] = -y_neg
+        y_verts[4::6] = y_pos
+        y_colors = np.concatenate([
+            darken(np.full(i.size, light)),
+            darken(np.full(j.size, light))]).repeat(6)
+
+        self._vertices = np.concatenate([axes_verts, x_verts, y_verts])
+        self._colors = np.concatenate([axes_colors, x_colors, y_colors])
+
+    def get_vertices_count(self):
+        return self._vertices.size // 3 if self._vertices.size else 0
+
+    def get_colors(self):
+        return self._colors if self._colors.size > 0 else None
+
+    def get_vertices(self):
+        return self._vertices if self._vertices.size > 0 else None
