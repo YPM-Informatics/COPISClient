c2746a2fe9e4317b0ffe275119767631aef7046f
XRuiz9 <xdruiz92@gmail.com>
2020-07-21 11:54:10 -0500

    Added dimensions spin controls

--- gui/pref_frame.py
+++ gui/pref_frame.py
@@ -20,9 +20,44 @@ class PreferenceFrame(wx.Frame):
         self.vbox1 = wx.BoxSizer(wx.VERTICAL)
         self.vbox1.Add((0,0))
 
-        build_dims_box = wx.BoxSizer
-        build_dims_label = wx.StaticText(self.panel, wx.ID_ANY, label='Set build dimensions', style=wx.ALIGN_LEFT)
-        build_dims_label.SetFont(self.font)
-        self.vbox1.Add(build_dims_label, 1, flag=wx.TOP|wx.BOTTOM|wx.LEFT, border=10)
+        # Build settings box
+        build_settings_box = wx.BoxSizer(wx.VERTICAL)
+        build_settings_label = wx.StaticText(self.panel, wx.ID_ANY, label='Customize your build settings', style=wx.ALIGN_LEFT)
+        build_settings_label.SetFont(self.font)
+        build_settings_box.Add(build_settings_label, 1, flag=wx.TOP|wx.BOTTOM|wx.LEFT, border=10)
+
+        # Dimensions Box
+        dims_box = wx.BoxSizer(wx.VERTICAL)
+        dims_label = wx.StaticText(self.panel, wx.ID_ANY, label='Dimensions', style=wx.ALIGN_LEFT)
+        dims_box.Add(dims_label, 1, flag = wx.BOTTOM, border=5)
+
+        width_box = wx.BoxSizer()
+        width_label = wx.StaticText(self.panel, wx.ID_ANY, label='Width: ')
+        width_box.Add(width_label)
+        self.width_sc = wx.SpinCtrl(self.panel, value='0', size=wx.Size(60, 22), min=0, max=1000)
+        width_box.Add(self.width_sc)
+
+        length_box = wx.BoxSizer()
+        length_label = wx.StaticText(self.panel, wx.ID_ANY, label='Length: ')
+        length_box.Add(length_label)
+        self.length_sc= wx.SpinCtrl(self.panel, value='0', size=wx.Size(60, 22), min=0, max=1000)
+        length_box.Add(self.length_sc)
+
+        height_box = wx.BoxSizer()
+        height_label =  wx.StaticText(self.panel, wx.ID_ANY, label='Height: ')
+        height_box.Add(height_label)
+        self.height_sc = wx.SpinCtrl(self.panel, value='0', size=wx.Size(60, 22), min=0, max=1000)
+        height_box.Add(self.height_sc)
+
+        dims_box.Add(width_box)
+        dims_box.Add(length_box)
+        dims_box.Add(height_box)
+
+        # Origin Box
+
+
+        build_settings_box.Add(dims_box)
+
+        self.vbox1.Add(build_settings_box, 1)
 
         self.panel.SetSizer(self.vbox1)
