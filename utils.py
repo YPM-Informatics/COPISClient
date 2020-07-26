c3726fee114de962dabd7d63ad676ba97d12d50d
k3vnchen <k3vnchen@gmail.com>
2020-07-26 02:10:00 -0400

    Load window width and height from config file

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@
 import wx
 import wx.svg as svg
 
+
 def set_dialog(msg):
     dialog = wx.MessageDialog(None, msg, 'Confirm Exit', wx.OK)
     dialog.ShowModal()
@@ -10,6 +11,5 @@ def set_dialog(msg):
 
 
 def svgbmp(svgimg, size):
-    # size = 64
     image = svg.SVGimage.CreateFromFile(svgimg).ConvertToScaledBitmap((size, size))
     return image
