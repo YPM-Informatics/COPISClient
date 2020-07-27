fb78270d05329f56ecc395c8388740f21d805b71
k3vnchen <k3vnchen@gmail.com>
2020-07-27 14:05:09 -0400

    Remove unused comments

--- gui/about.py
+++ gui/about.py
@@ -5,17 +5,17 @@ import wx
 
 class AboutDialog(wx.Dialog):
     def __init__(self, parent):
-        wx.Dialog.__init__ (self, parent, id=wx.ID_ANY, title=u"About", pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.DEFAULT_DIALOG_STYLE)
+        wx.Dialog.__init__ (self, parent, id=wx.ID_ANY, title='About', pos=wx.DefaultPosition, size=wx.DefaultSize, style=wx.DEFAULT_DIALOG_STYLE)
 
-        self.SetSizeHints((200, 100), wx.DefaultSize)
+        self.SetSizeHints(wx.Size(200, 100), wx.DefaultSize)
 
         sizer = wx.BoxSizer(wx.VERTICAL)
 
         statictext = wx.StaticText(self, wx.ID_ANY, 'Under Construction', wx.DefaultPosition, wx.DefaultSize, 0)
         sizer.Add(statictext, 1, wx.ALIGN_CENTER_HORIZONTAL | wx.ALL, 5)
 
-        self.SetSizer(sizer);
-        self.Layout();
+        self.SetSizer(sizer)
+        self.Layout()
 
     def __del__(self):
           pass
