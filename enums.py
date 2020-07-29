dbe716758d8ba567cca572107c7fe7104ee6b9ad
k3vnchen <k3vnchen@gmail.com>
2020-07-29 12:17:56 -0400

    Create customizable cube

--- enums.py
+++ enums.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from enum import Enum
+from enum import Enum, IntEnum
 
 
 class ToolIds(Enum):
-    PLAY     = 1
-    PAUSE    = 2
-    STOP     = 3
+    PLAY = 1
+    PAUSE = 2
+    STOP = 3
     SETTINGS = 4
 
 
@@ -23,3 +23,18 @@ class CamAxis(Enum):
 class CamMode(Enum):
     NORMAL = 'normal'
     ROTATE = 'rotate'
+
+
+class ViewCubePos(Enum):
+    TOP_LEFT = 0
+    TOP_RIGHT = 1
+    BOTTOM_LEFT = 2
+    BOTTOM_RIGHT = 3
+
+
+class ViewCubeSize(IntEnum):
+    SMALLER = 85
+    SMALL = 100
+    MEDIUM = 115
+    LARGE = 140
+    LARGER = 170
