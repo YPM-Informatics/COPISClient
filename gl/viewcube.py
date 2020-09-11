0f1e03ef6df9fdaca62392f5ba512b825c0ecc7a
k3vnchen <k3vnchen@gmail.com>
2020-09-11 15:02:18 -0400

    Hotfix

--- gl/viewcube.py
+++ gl/viewcube.py
@@ -239,7 +239,7 @@ class GLViewCube:
             corner = (0, 0)
         else: # self._position == ViewCubePos.BOTTOM_LEFT:
             corner = (width - self._size, 0)
-        return *corner, self._size, self._size
+        return corner[0], corner[1], self._size, self._size
 
     @property
     def hover_id(self) -> int:
