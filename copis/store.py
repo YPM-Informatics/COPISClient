f165aaa3a7f0212d241fe7390b256c21c27e0b9b
Djihbrihou Abibou <djihbril@outlook.com>
2021-04-12 00:58:56 -0400

    Cleaning up the store class

--- copis/store.py
+++ copis/store.py
@@ -13,16 +13,17 @@
 # You should have received a copy of the GNU General Public License
 # along with COPISClient.  If not, see <https://www.gnu.org/licenses/>.
 
+"""COPIS Application Data storage facility."""
+
+__version__ = ""
+
 import os
 import pickle
 
 from pathlib import PurePath
 from configparser import ConfigParser
-from typing import List
 
 from settings import Settings
-from session_image import SessionImage
-
 
 class Store():
     """Handles application-wide data storage operations."""
@@ -32,8 +33,6 @@ class Store():
     _CONFIG_FOLDER = 'config'
     _CONFIG_FILE = 'app.ini'
 
-    # _SESSION_DIR = os.path.join(_CONFIG_DIR, 'sessions')
-
 
     def __init__(self) -> None:
         current = os.path.dirname(__file__)
@@ -42,25 +41,23 @@ class Store():
         root_segments = segments[1:index]
 
         root = '/' + PurePath(os.path.join(*root_segments)).as_posix()
-        
+
         self._root_dir = root
         self._config_dir = os.path.join(root, self._PROJECT_FOLDER, self._CONFIG_FOLDER)
         self._config_path = os.path.join(self._config_dir, self._CONFIG_FILE)
-        # self._session_dir = self._SESSION_DIR
 
         if not os.path.exists(self._config_dir):
             os.makedirs(self._config_dir)
-        
-        # if not os.path.exists(self._session_dir:
-        #     os.makedirs(self._session_dir)
 
 
     def save_config(self, parser: ConfigParser) -> None:
-        with open(self._config_path, 'w') as f:
-            parser.write(f)
+        """Saves a configuration object to file."""
+        with open(self._config_path, 'w') as file:
+            parser.write(file)
 
 
     def save_config_settings(self, settings: Settings) -> None:
+        """Saves a configuration object to file, via its settings object."""
         parser = ConfigParser()
         parser.read_dict(settings.as_dict())
 
@@ -68,46 +65,25 @@ class Store():
 
 
     def load_config(self) -> ConfigParser:
+        """Load a configuration object from file"""
         if os.path.exists(self._config_path):
             parser = ConfigParser()
             parser.read(self._config_path)
 
             return parser
-        else:
-            return None
+
+        return None
 
 
     def save(self, filename: str, obj: object) -> None:
+        """Saves an object to file"""
         with open(filename, 'wb') as file:
             pickle.dump(obj, file)
 
 
     def load(self, filename: str, obj: object) -> object:
+        """Loads as object from file"""
         with open(filename, 'rb') as file:
             obj = pickle.load(file)
 
         return obj
-
-    def create_session_path(self) -> str:
-        """Returns new session path. Checks folder names first"""
-        pass
-
-
-    def check_session_path(self) -> bool:
-        """Verifies that a session path exists"""
-        pass
-
-
-    def save_session_script(self, filename: str) -> None:
-        """Saves session script, which is the action list"""
-        pass
-
-
-    def load_session_script(self, filename: str) -> None:
-        """Saves session script, which is the action list"""
-        pass
-
-
-    def save_session_images(self, images: List[SessionImage]) -> None:
-        """Saves session images and image metadata"""
-        pass
