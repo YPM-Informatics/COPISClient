Fix viewcube rotation

copis/gl/viewcube.py
@@ -78,13 +78,13 @@ class GLViewCube:
 
         vertices = np.array([
             1.0, 1.0, 1.0, 0.7, 0.7, 0.7,
-            1.0, -1.0, 1.0, 0.5, 0.5, 0.5,
-            1.0, -1.0, -1.0, 0.5, 0.5, 0.5,
-            1.0, 1.0, -1.0, 0.7, 0.7, 0.7,
-            -1.0, 1.0, -1.0, 0.7, 0.7, 0.7,
+            1.0, -1.0, 1.0, 0.7, 0.7, 0.7,
+            1.0, -1.0, -1.0, 0.4, 0.4, 0.4,
+            1.0, 1.0, -1.0, 0.4, 0.4, 0.4,
+            -1.0, 1.0, -1.0, 0.4, 0.4, 0.4,
             -1.0, 1.0, 1.0, 0.7, 0.7, 0.7,
-            -1.0, -1.0, 1.0, 0.5, 0.5, 0.5,
-            -1.0, -1.0, -1.0, 0.5, 0.5, 0.5,
+            -1.0, -1.0, 1.0, 0.7, 0.7, 0.7,
+            -1.0, -1.0, -1.0, 0.4, 0.4, 0.4,
         ], dtype=np.float32)
         indices = np.array([
             0, 1, 2, 2, 3, 0,
@@ -112,35 +112,35 @@ class GLViewCube:
         # --- viewcube for picking ---
 
         vertices = np.array([
-            1.0, 1.0, 1.0,      # 0 front   (id = 0)
-            -1.0, 1.0, 1.0,     # 5
-            -1.0, -1.0, 1.0,    # 6
-            1.0, -1.0, 1.0,     # 1
-
-            1.0, 1.0, 1.0,      # 0 top     (id = 1)
-            1.0, 1.0, -1.0,     # 3
-            -1.0, 1.0, -1.0,    # 4
-            -1.0, 1.0, 1.0,     # 5
-
-            1.0, 1.0, 1.0,      # 0 right   (id = 2)
-            1.0, -1.0, 1.0,     # 1
-            1.0, -1.0, -1.0,    # 2
-            1.0, 1.0, -1.0,     # 3
-
-            1.0, -1.0, 1.0,     # 1 bottom  (id = 3)
+            1.0, -1.0, 1.0,     # 1 front  (id = 0)
             -1.0, -1.0, 1.0,    # 6
             -1.0, -1.0, -1.0,   # 7
             1.0, -1.0, -1.0,    # 2
 
-            -1.0, -1.0, -1.0,   # 7 left    (id = 4)
+            1.0, 1.0, 1.0,      # 0 top    (id = 1)
+            -1.0, 1.0, 1.0,     # 5
+            -1.0, -1.0, 1.0,    # 6
+            1.0, -1.0, 1.0,     # 1
+
+            1.0, 1.0, 1.0,      # 0 right  (id = 2)
+            1.0, -1.0, 1.0,     # 1
+            1.0, -1.0, -1.0,    # 2
+            1.0, 1.0, -1.0,    # 3
+
+            -1.0, -1.0, -1.0,   # 7 bottom (id = 3)
+            -1.0, 1.0, -1.0,    # 4
+            1.0, 1.0, -1.0,     # 3
+            1.0, -1.0, -1.0,    # 2
+
+            -1.0, -1.0, -1.0,   # 7 left   (id = 4)
             -1.0, -1.0, 1.0,    # 6
             -1.0, 1.0, 1.0,     # 5
             -1.0, 1.0, -1.0,    # 4
 
-            -1.0, -1.0, -1.0,   # 7 back    (id = 5)
-            -1.0, 1.0, -1.0,    # 4
+            1.0, 1.0, 1.0,      # 0 back   (id = 5)
             1.0, 1.0, -1.0,     # 3
-            1.0, -1.0, -1.0,    # 2
+            -1.0, 1.0, -1.0,    # 4
+            -1.0, 1.0, 1.0,     # 5
         ], dtype=np.float32)
         colors = np.zeros(72, dtype=np.float32)
         colors[::3] = np.arange(6).repeat(4) / 255.0
@@ -197,7 +197,10 @@ class GLViewCube:
         glViewport(*self._get_viewport())
 
         proj = glm.ortho(-2.3, 2.3, -2.3, 2.3, -2.3, 2.3)
-        modelview = glm.mat4_cast(self.parent.rot_quat)
+        mat = glm.lookAt(glm.vec3(0.0, -1.0, 0.0),               # position
+                         glm.vec3(0.0, 0.0, 0.0),                # target
+                         glm.vec3(0.0, 0.0, 1.0))                # up
+        modelview = mat * glm.mat4_cast(self.parent.rot_quat)
 
         glUseProgram(self.parent.shaders['default'])
         glUniformMatrix4fv(0, 1, GL_FALSE, glm.value_ptr(proj))
@@ -219,7 +222,10 @@ class GLViewCube:
         glViewport(*self.viewport)
 
         proj = glm.ortho(-2.3, 2.3, -2.3, 2.3, -2.3, 2.3)
-        modelview = glm.mat4_cast(self.parent.rot_quat)
+        mat = glm.lookAt(glm.vec3(0.0, -1.0, 0.0),               # position
+                         glm.vec3(0.0, 0.0, 0.0),                # target
+                         glm.vec3(0.0, 0.0, 1.0))                # up
+        modelview = mat * glm.mat4_cast(self.parent.rot_quat)
 
         glUseProgram(self.parent.shaders['default'])
         glUniformMatrix4fv(0, 1, GL_FALSE, glm.value_ptr(proj))