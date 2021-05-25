e79947051eebd1c3195d29a1fbdc45ae8466f68f
Djihbrihou Abibou <djihbril@outlook.com>
2021-05-25 08:18:25 -0400

    Adding a reading thread for each open connection

--- copis/coms/serial_controller.py
+++ copis/coms/serial_controller.py
@@ -15,13 +15,17 @@
 
 """Manages COPIS Serial Communications"""
 
+
 from dataclasses import dataclass
 from typing import List
 
 import serial
+from serial.serialutil import SerialException
 
 from serial.tools import list_ports
 from mprop import mproperty
+
+from .serial_reading_thread import SerialReadingThread
 
 
 @dataclass
@@ -45,8 +49,9 @@ class SerialController():
     """Implement Serial Functionalities"""
 
     _TEST_SERIAL_PORT = 'TEST'
+    _READ_TIMEOUT = 1
 
-    BAUDS = [ 9600, 19200, 38400, 57600, 115200]
+    BAUDS = [9600, 19200, 38400, 57600, 115200]
 
     def __init__(self):
         self._ports = []
@@ -54,7 +59,7 @@ class SerialController():
         self._console = None
         self._is_dev_env = False
 
-    def initialize(self, console, is_dev_env: bool = False) -> None:
+    def initialize(self, console = None, is_dev_env: bool = False) -> None:
         """Initialize the serial object"""
         if any(p.is_open for p in self._ports):
             return
@@ -70,12 +75,12 @@ class SerialController():
         has_active_port = self._active_port is not None
 
         if port is None:
-            self._console.print('Invalid attempt to select unknown port.')
+            self._print('Invalid attempt to select unknown port.')
             return None
 
         if has_active_port and port.name == self._active_port.name \
             and self._active_port is not None:
-            self._console.print('Port already selected.')
+            self._print('Port already selected.')
             return port
 
         if has_active_port:
@@ -100,7 +105,7 @@ class SerialController():
 
             return port
         except serial.SerialException as err:
-            self._console.print(f'Error instantiating serial connection: {err.args[0]}')
+            self._print(f'Error instantiating serial connection: {err.args[0]}')
             return None
 
     def open_port(self, baud: int = BAUDS[-1]) -> bool:
@@ -109,15 +114,23 @@ class SerialController():
         has_active_port = active_port is not None
 
         if not has_active_port:
-            self._console.print('No port selected.')
+            self._print('No port selected.')
             return False
 
         if active_port.connection.is_open:
-            self._console.print('Port connection already open.')
+            self._print('Port connection already open.')
             return True
 
-        active_port.connection.baudrate = baud
-        active_port.connection.open()
+        try:
+            active_port.connection.baudrate = baud
+            active_port.connection.timeout = self._READ_TIMEOUT
+            active_port.connection.open()
+        except SerialException as err:
+            self._print(err.args[0])
+            return False
+
+        SerialReadingThread(active_port.connection).start()
+
         return True
 
     def close_port(self) -> None:
@@ -136,6 +149,7 @@ class SerialController():
             and active_port.connection.is_open:
             data = data.rstrip("\r")
             data = f'{data}\r'.encode()
+
             active_port.connection.write(data)
 
     def terminate(self) -> None:
@@ -143,18 +157,6 @@ class SerialController():
         for port in self._ports:
             if port is not None and port.connection is not None and port.connection.is_open:
                 port.connection.close()
-
-    @property
-    def is_port_open(self) -> bool:
-        """Returns open status of the active port"""
-        return self._active_port.connection.is_open \
-            if self._active_port is not None and self._active_port.connection is not None else False
-
-    @property
-    def port_list(self) -> List[SerialPort]:
-        """Returns a copy of the serial ports list"""
-
-        return self._ports.copy()
 
     def update_port_list(self) -> None:
         """Updates the serial ports list"""
@@ -189,12 +191,26 @@ class SerialController():
             any(p.name == self._active_port.name for p in self._ports):
             self._active_port = None
 
+    def _print(self, msg):
+        print(msg) if self._console is None else self._console.print(msg)
+
     def _get_port(self, name: str):
-        """Finds a serial port by name in the list of ports and returns it, or None"""
         if len(self._ports) < 1:
             return None
 
         return next(filter(lambda p, n = name: p.name == n, self._ports), None)
+
+    @property
+    def is_port_open(self) -> bool:
+        """Returns open status of the active port"""
+        return self._active_port.connection.is_open \
+            if self._active_port is not None and self._active_port.connection is not None else False
+
+    @property
+    def port_list(self) -> List[SerialPort]:
+        """Returns a copy of the serial ports list"""
+
+        return self._ports.copy()
 
 
 _instance = SerialController()
---
+++ copis/coms/serial_reading_thread.py
@@ -0,0 +1,73 @@
+# This file is part of COPISClient.
+#
+# COPISClient is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# COPISClient is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with COPISClient.  If not, see <https://www.gnu.org/licenses/>.
+
+"""Provides COPIS Serial Reading Thread"""
+
+
+import ast
+import threading
+import time
+
+from serial import Serial
+
+class SerialReadingThread(threading.Thread):
+    """Implements the ability to continuously read a serial connection's output"""
+
+    def __init__(self, connection: Serial):
+        threading.Thread.__init__(self)
+        self._connection = connection
+        self._response_stack = []
+
+    def run(self):
+        while self._connection.is_open:
+            time.sleep(0.001)
+            while self._connection.is_open and self._connection.in_waiting > 0:
+                p_bytes = self._connection.readline().decode('utf-8')
+                print(p_bytes)
+                self._parse_output(p_bytes)
+
+                if self._connection.in_waiting == 0:
+                    idle_response = \
+                        next(filter(lambda r: r['ssf'] == 0, self._response_stack), None)
+                    
+                    if idle_response is not None:
+                        #TODO: broadcast OK.
+                        #TODO: fix serial.serialutil.SerialException: ClearCommError failed (OSError(9, 'The handle is invalid.', None, 6))
+                        # that occurs on exit.
+                        pass
+
+                    self._response_stack.clear()
+
+    def _parse_output(self, data: str) -> None:
+        data = data.strip('\r\n')
+        if data.startswith('<') and data.endswith('>'):
+            data = data.lstrip('<').rstrip('>')
+
+            segments = data.split(',', 2)
+            segments = [tuple(s.split(':')) for s in segments]
+            mapped = map(_stringify, segments)
+
+            result = '{' + ', '.join(mapped) + '}'
+
+            self._response_stack.append(ast.literal_eval(result))
+        else:
+            return
+
+def _stringify(data):
+    if ',' in data[1]:
+        value = '{{"X": {0}, "Y": {1}, "Z": {2}, "P": {3}, "T": {4}}}'.format(*data[1].split(','))
+        return f'"{data[0]}": {value}'
+
+    return f'"{data[0]}": {data[1]}'
