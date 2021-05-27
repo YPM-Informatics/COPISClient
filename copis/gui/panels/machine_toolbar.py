8affe9b84be79c65338a8dc56e30417b1722d1a6
Djihbrihou Abibou <djihbril@outlook.com>
2021-05-26 22:11:34 -0400

    Playing the action list to the serial port
    WE CAN NOW PLAY A PATH ON THE MACHINE!!!!!!

--- copis/gui/panels/machine_toolbar.py
+++ copis/gui/panels/machine_toolbar.py
@@ -16,6 +16,7 @@
 """MachineToolbar class."""
 
 import logging
+import threading
 import wx
 import wx.lib.agw.aui as aui
 
@@ -31,9 +32,9 @@ class MachineToolbar(aui.AuiToolBar):
         """Inits MachineToolbar with constructors."""
         super().__init__(parent, style=wx.BORDER_DEFAULT, agwStyle=
             aui.AUI_TB_PLAIN_BACKGROUND|aui.AUI_TB_OVERFLOW)
-        self.parent = parent
-        self.core = self.parent.core
-
+        self._parent = parent
+        self._core = self._parent.core
+        self._serial = self._core.serial
         self._console = console
 
         self.port_cb = None
@@ -73,13 +74,16 @@ class MachineToolbar(aui.AuiToolBar):
         # Add play, pause, stop tools.
         _bmp = create_scaled_bitmap('play_arrow', 24)
         self.AddTool(ToolIds.PLAY.value, 'Play', _bmp, _bmp, aui.ITEM_NORMAL,
-            short_help_string='Play simulation')
-        _bmp = create_scaled_bitmap('pause', 24)
-        self.AddTool(ToolIds.PAUSE.value, 'Pause', _bmp, _bmp, aui.ITEM_NORMAL,
-            short_help_string='Pause simulation')
-        _bmp = create_scaled_bitmap('stop', 24)
-        self.AddTool(ToolIds.STOP.value, 'Stop', _bmp, _bmp, aui.ITEM_NORMAL,
-            short_help_string='Stop and reset simulation')
+            short_help_string='Start imaging')
+
+        # TODO: implement pause and stop, uncomment below
+        # _bmp = create_scaled_bitmap('pause', 24)
+        # self.AddTool(ToolIds.PAUSE.value, 'Pause', _bmp, _bmp, aui.ITEM_NORMAL,
+        #     short_help_string='Pause simulation')
+        # _bmp = create_scaled_bitmap('stop', 24)
+        # self.AddTool(ToolIds.STOP.value, 'Stop', _bmp, _bmp, aui.ITEM_NORMAL,
+        #     short_help_string='Stop and reset simulation')
+
         _bmp = create_scaled_bitmap('get_app', 24)
         self.AddTool(ToolIds.EXPORT.value, 'Export', _bmp, _bmp, aui.ITEM_NORMAL,
             short_help_string='Export actions as text')
@@ -103,7 +107,7 @@ class MachineToolbar(aui.AuiToolBar):
         self.connect_btn.Label = 'Connect'
 
         self._print('Refreshing serial ports...')
-        self.core.serial.update_port_list()
+        self._serial.update_port_list()
         self._print('Serial ports refreshed.')
 
         self.update_ports()
@@ -116,12 +120,12 @@ class MachineToolbar(aui.AuiToolBar):
             self.baud_cb.Selection = -1
             self.baud_cb.Items = []
         else:
-            self.baud_cb.Items = [str(i) for i in self.core.serial.BAUDS]
+            self.baud_cb.Items = [str(i) for i in self._serial.BAUDS]
             self.baud_cb.Selection = len(self.baud_cb.Items) - 1
 
             selection = self.port_cb.Selection
             p_name = self.port_cb.Items[selection]
-            port = self.core.serial.select_port(p_name)
+            port = self._serial.select_port(p_name)
             self.connect_btn.Label = 'Disconnect' \
                 if port.connection is not None and port.connection.is_open else 'Connect'
 
@@ -131,23 +135,23 @@ class MachineToolbar(aui.AuiToolBar):
         if self.port_cb.Selection >= 0:
             selection = self.baud_cb.Selection
             baud = int(self.baud_cb.Items[selection]) \
-                if selection >= 0 else self.core.serial.BAUDS[-1]
+                if selection >= 0 else self._serial.BAUDS[-1]
 
             if connect_btn.Label == 'Connect':
-                if self.core.serial.open_port(baud):
+                if self._serial.open_port(baud):
                     connect_btn.Label = 'Disconnect'
                 else:
                     connect_btn.Label = 'Connect'
                     set_dialog('Unable to connect.')
             else:
-                self.core.serial.close_port()
+                self._serial.close_port()
                 connect_btn.Label = 'Connect'
         else:
             set_dialog('Please select a port to connect to.')
 
     def update_ports(self) -> None:
         """Set port combobox to serial ports list."""
-        self.port_cb.Items = [p.name for p in self.core.serial.port_list]
+        self.port_cb.Items = [p.name for p in self._serial.port_list]
 
     def on_tool_selected(self, event: wx.CommandEvent) -> None:
         """On toolbar tool selected, check which and process accordingly.
@@ -155,29 +159,66 @@ class MachineToolbar(aui.AuiToolBar):
         TODO: Link with copiscore when implemented.
         """
         if event.Id == ToolIds.PLAY.value:
-            if self.core.paused:
-                self.core.resume()
-            else:
-                self.core.start_imaging()
+            # if self.core.paused:
+            #     self.core.resume()
+            # else:
+            #     self.core.start_imaging()
+
+            is_connected = self._serial.is_port_open
+            has_path = len(self._core.actions)
+            can_image = is_connected and has_path
+
+            if not can_image:
+                msg = 'The machine needs to be connected for imaging.' \
+                    if not is_connected else 'The machine needs a path for imaging.'
+                set_dialog(msg)
+                return
+
+            actions = self._core.export_actions()
+
+            imaging_thread = threading.Thread(name='imaging thread', target=self._run_imaging,
+                kwargs={'actions': actions}, daemon=True)
+            imaging_thread.start()
 
         elif event.Id == ToolIds.PAUSE.value:
-            self.core.pause()
+            self._core.pause()
 
         elif event.Id == ToolIds.STOP.value:
-            self.core.cancel_imaging()
+            self._core.cancel_imaging()
 
         elif event.Id == ToolIds.SETTINGS.value:
             with MachineSettingsDialog(self) as dlg:
                 logging.debug('Machine Settings opened')
 
         elif event.Id == ToolIds.EXPORT.value:
-            self.core.export_actions('actions.txt')
+            self._core.export_actions('actions.txt')
 
     def _print(self, msg):
         if self._console is None:
             print(msg)
         else:
             self._console.print(msg)
+
+    def _run_imaging(self, actions):
+        self._print('Imaging started')
+
+        for i, action in enumerate(actions):
+            self._print(f'Sending: {action}')
+            response = self._serial.write(action)
+
+            last = response[-1]
+            if isinstance(last, dict):
+                if last['is_idle']:
+                    self._print(
+                        f'Received OK.{"" if i == len(actions) - 1 else " Ready for next command."}'
+                    )
+                else:
+                    self._print('Idle not detected. imaging stopped.')
+                    break
+            else:
+                self._print(f'Received: {response}')
+
+        self._print('Imaging ended')
 
     def __del__(self) -> None:
         pass
