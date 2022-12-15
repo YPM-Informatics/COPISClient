f5c4a569a11f47f8f30f1b4483ff5184610813e6
djihbril <djihbril@outlook.com>
2022-12-15 14:23:27 -0500

    Repatriating component members to core

--- copis/core/__init__.py
+++ copis/core/__init__.py
@@ -29,6 +29,7 @@ import threading
 import warnings
 import uuid
 
+from random import shuffle as rand_shuffle
 from typing import List, Tuple
 from datetime import datetime
 from itertools import groupby, zip_longest
@@ -36,28 +37,32 @@ from glm import vec2, vec3
 from pydispatch import dispatcher
 
 from copis.command_processor import serialize_command
-from copis.helpers import get_atype_kind, get_timestamp, print_error_msg, print_debug_msg, print_info_msg
-from copis.globals import ActionType, ComStatus, DebugEnv, WorkType
+from copis.helpers import get_atype_kind, print_error_msg, print_debug_msg, print_info_msg, create_action_args, get_action_args_values, get_end_position, get_heading, sanitize_number
+from copis.globals import ActionType, ComStatus, DebugEnv, Point5, WorkType
 from copis.config import Config
 from copis.project import Project
-from copis.classes import MonitoredList, SerialResponse
+from copis.classes import Action, MonitoredList, SerialResponse
 from copis import store
 from copis.classes.sys_db import SysDB
+from copis.mathutils import optimize_rotation_move_to_angle
 
 from ._console_output import ConsoleOutput
 from ._machine_members import MachineMembersMixin
-from ._component_members import ComponentMembersMixin
 from ._communication_members import CommunicationMembersMixin
 
 
 class COPISCore(
     MachineMembersMixin,
-    ComponentMembersMixin,
     CommunicationMembersMixin):
     """COPISCore. Connects and interacts with devices in system."""
 
     _YIELD_TIMEOUT = .001 # 1 millisecond
     _IMAGING_MANIFEST_FILE_NAME = 'copis_imaging_manifest.json'
+    MOVE_COMMANDS = [ActionType.G0, ActionType.G1]
+    F_STACK_COMMANDS = [ActionType.HST_F_STACK, ActionType.EDS_F_STACK]
+    SNAP_COMMANDS = [ActionType.C0, ActionType.EDS_SNAP]
+    FOCUS_COMMANDS = [ActionType.C1, ActionType.EDS_FOCUS]
+    LENS_COMMANDS = SNAP_COMMANDS + FOCUS_COMMANDS
 
     def __init__(self, parent=None) -> None:
         """Initializes a COPISCore instance."""
@@ -119,6 +124,35 @@ class COPISCore(
         self._ressetable_send_delay_ms = 0 #delay time in milliseconds before sending a serial command after recieiving idle/CTS
 
     @property
+    def save_imaging_session(self) -> str:
+        """Returns a flag indicating whether to save the imaging session."""
+        return self._save_imaging_session
+
+    @property
+    def imaging_session_path(self) -> str:
+        """Returns the user-defined imaging session folder location."""
+        return self._imaging_session_path
+
+    @property
+    def imaging_target(self) -> vec3:
+        """Returns the coordinates of the last target; (0,0,0) if application just started."""
+        return self._imaging_target
+
+    @imaging_target.setter
+    def imaging_target(self, value: vec3) -> None:
+        self._imaging_target = value
+
+    @property
+    def selected_pose(self) -> int:
+        """Returns the selected pose's ID."""
+        return self._selected_pose
+
+    @property
+    def selected_pose_set(self) -> int:
+        """Returns the selected pose set's ID."""
+        return self._selected_pose_set
+
+    @property
     def imaged_pose_sets(self):
         """Returns a list of pose set indexes that have been imaged
             in the current run."""
@@ -134,6 +168,9 @@ class COPISCore(
     def is_dev_env(self):
         """Returns a flag indicating whether we are in a dev environment."""
         return self._is_dev_env
+
+    def _get_device(self, device_id):
+        return next(filter(lambda d: d.device_id == device_id, self.project.devices), None)
 
     def _listener(self) -> None:
         """Implements a listening thread."""
@@ -518,6 +555,210 @@ class COPISCore(
         #self._session_id +=1
         dispatcher.disconnect(self._on_device_ser_updated, signal='ntf_device_ser_updated')
         dispatcher.disconnect(self._on_device_eds_updated, signal='ntf_device_eds_updated')
+
+    def select_proxy(self, index) -> None:
+        """Selects proxy given index in proxy list."""
+        if index < 0:
+            if self._selected_proxy >= 0:
+                self._selected_proxy = -1
+
+                dispatcher.send('ntf_o_deselected')
+        elif index < len(self.project.proxies):
+            self.select_device(-1)
+            self.select_pose(-1)
+            self.select_pose_set(-1)
+
+            self._selected_proxy = index
+
+            dispatcher.send('ntf_o_selected', object=self._selected_proxy)
+        else:
+            print_error_msg(self.console, f'Proxy object index {index} is out of range.')
+
+    def select_device(self, index: int) -> None:
+        """Selects device given index in device list."""
+        if index < 0:
+            if self._selected_device >= 0:
+                self._selected_device = -1
+
+                dispatcher.send('ntf_d_deselected')
+        elif index < len(self.project.devices):
+            self.select_proxy(-1)
+            self.select_pose(-1)
+            self.select_device(-1)
+            self.select_pose_set(-1)
+
+            self._selected_device = index
+
+            dispatcher.send('ntf_d_selected', device=self.project.devices[self._selected_device])
+        else:
+            print_error_msg(self.console, f'Device index {index} is out of range.')
+
+    def select_pose_set(self, index: int) -> None:
+        """Highlights poses in a set given pose set index."""
+        if index < 0:
+            selected = self._selected_pose_set
+            if self._selected_pose_set >= 0:
+                self._selected_pose_set = -1
+
+                dispatcher.send('ntf_s_deselected', set_index=selected)
+        elif index < len(self.project.pose_sets):
+            self.select_device(-1)
+            self.select_proxy(-1)
+            self.select_pose(-1)
+            self.select_pose_set(-1)
+
+            self._selected_pose_set = index
+
+            dispatcher.send('ntf_s_selected', set_index=self._selected_pose_set)
+        else:
+            print_error_msg(self.console, f'Pose set index {index} is out of range.')
+
+    def select_pose(self, index: int) -> None:
+        """Selects pose given index in pose list."""
+        if index < 0:
+            selected = self._selected_pose
+            if self._selected_pose >= 0:
+                self._selected_pose = -1
+                dispatcher.send('ntf_a_deselected', pose_index=selected)
+        elif index < len(self.project.poses):
+            self.select_device(-1)
+            self.select_proxy(-1)
+            self.select_pose(-1)
+            self.select_pose_set(-1)
+
+            self._selected_pose = index
+
+            dispatcher.send('ntf_a_selected', pose_index=self._selected_pose)
+        else:
+            print_error_msg(self.console, f'Pose index {index} is out of range.')
+
+    def update_selected_pose_position(self, args) -> None:
+        """Update position of selected pose."""
+        args = create_action_args(args)
+        pose_position = self.project.poses[self._selected_pose].position
+        argc = min(len(pose_position.args), len(args))
+
+        for i in range(argc):
+            pose_position.args[i] = args[i]
+
+        pose_position.argc = argc
+        pose_position.update()
+
+        dispatcher.send('ntf_a_list_changed')
+
+    def re_target_all_poses(self) -> None:
+        """Re-targets all the poses with the given heading."""
+        for pose in self.project.poses:
+            pose_position = pose.position
+            args = get_action_args_values(pose_position.args)
+            end_pan, end_tilt = get_heading(vec3(args[:3]), self.imaging_target)
+
+            args[3] = sanitize_number(end_pan)
+            args[4] = sanitize_number(end_tilt)
+            args = create_action_args(args)
+
+            argc = min(len(pose_position.args), len(args))
+
+            for i in range(argc):
+                pose_position.args[i] = args[i]
+
+            pose_position.argc = argc
+            pose_position.update()
+
+        dispatcher.send('ntf_a_list_changed')
+
+    def target_vector_step_all_poses(self, distance) -> None:
+        """Steps all poses towards/away from the target."""
+        for pose in self.project.poses:
+            pose_position = pose.position
+            args = get_action_args_values(pose_position.args)
+
+            end = get_end_position(Point5(*args[:5]), distance)
+            end_pan, end_tilt = get_heading(end, self.imaging_target)
+
+            args[0] = end.x
+            args[1] = end.y
+            args[2] = end.z
+            args[3] = sanitize_number(end_pan)
+            args[4] = sanitize_number(end_tilt)
+            args = create_action_args(args)
+
+            argc = min(len(pose_position.args), len(args))
+
+            for i in range(argc):
+                pose_position.args[i] = args[i]
+
+            pose_position.argc = argc
+            pose_position.update()
+
+        dispatcher.send('ntf_a_list_changed')
+
+    def add_to_selected_pose_payload(self, item: Action) -> bool:
+        """Appends an action to the selected pose's payload."""
+        pose = self.project.poses[self._selected_pose]
+        pose.payload.append(item)
+
+        dispatcher.send('ntf_a_list_changed')
+
+        return True
+
+    def delete_from_selected_pose_payload(self, index: int) -> bool:
+        """Deletes an action from the selected pose's payload, given an index."""
+        pose = self.project.poses[self._selected_pose]
+        pose.payload.pop(index)
+
+        dispatcher.send('ntf_a_list_changed')
+
+        return True
+
+    def export_poses(self, filename: str = None) -> list:
+        """Serialize action list and write to file.
+
+        TODO: Expand to include not just G0 and C0 actions
+        """
+
+        lines = []
+
+        for pose in self.project.poses:
+            line = serialize_command(pose)
+            lines.append(line)
+
+        if filename is not None:
+            with open(filename, 'w', encoding='utf-8') as file:
+                file.write('\n'.join(lines))
+
+        dispatcher.send('ntf_a_exported', filename=filename)
+        return lines
+
+    def optimize_all_poses_pan_angles(self) -> None:
+        """Optimizes all the poses to minimize panning motion cost."""
+        last_position_by_id = {}
+        for pose in self.project.poses:
+            pose_position = pose.position
+            device_id = pose[0].device
+            args = get_action_args_values(pose_position.args)
+            pt = Point5(*args[:5])
+            if device_id not in last_position_by_id:
+                last_position_by_id[device_id] = pt
+            else:
+                optimized_pan_angle = optimize_rotation_move_to_angle(last_position_by_id[device_id].p, pt.p)
+                args[3] = sanitize_number(optimized_pan_angle)
+                args = create_action_args(args)
+                argc = min(len(pose_position.args), len(args))
+                for i in range(argc):
+                    pose_position.args[i] = args[i]
+                pose_position.argc = argc
+                pose_position.update()
+                args = get_action_args_values(pose_position.args)
+                pt = Point5(*args[:5])
+                last_position_by_id[device_id] = pt
+        dispatcher.send('ntf_a_list_changed')
+        #self.select_pose(self.selected_pose) # Reselect the current selected pose (if one was selected) to update variables in transform panel.
+
+    def optimize_all_poses_randomize(self) -> None:
+        """Optimizes all the poses to minimize panning motion cost."""
+        rand_shuffle(self.project.pose_sets)
+        dispatcher.send('ntf_a_list_changed')
 
     def start_new_project(self) -> None:
         """Starts a new project with defaults."""
