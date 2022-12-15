178392d6ba729a21d9a0006c17e426660e42e698
djihbril <djihbril@outlook.com>
2022-12-15 15:39:13 -0500

    Repatriating machine members to core

--- copis/core/__init__.py
+++ copis/core/__init__.py
@@ -40,7 +40,7 @@ from pydispatch import dispatcher
 
 from canon.EDSDKLib import EvfDriveLens
 from copis.coms import serial_controller
-from copis.command_processor import serialize_command
+from copis.command_processor import deserialize_command, serialize_command
 from copis.helpers import get_atype_kind, print_error_msg, print_debug_msg, print_info_msg, create_action_args, get_action_args_values, get_end_position, get_heading, sanitize_number, locked
 from copis.globals import ActionType, ComStatus, DebugEnv, Point5, WorkType
 from copis.config import Config
@@ -51,11 +51,9 @@ from copis.classes.sys_db import SysDB
 from copis.mathutils import optimize_rotation_move_to_angle
 
 from ._console_output import ConsoleOutput
-from ._machine_members import MachineMembersMixin
 
 
-class COPISCore(
-    MachineMembersMixin):
+class COPISCore:
     """COPISCore. Connects and interacts with devices in system."""
 
     _YIELD_TIMEOUT = .001 # 1 millisecond
@@ -73,7 +71,7 @@ class COPISCore(
         print ("using profile: ", store.get_profile_path())
 
         self.sys_db = SysDB()
-        self._session_id = self.sys_db.last_session_id() +1
+        self._session_id = self.sys_db.last_session_id() + 1
         self._session_guid = str(uuid.uuid4()) # Global session if, useful is merging dbs.
 
         self.project = Project()
@@ -124,6 +122,69 @@ class COPISCore(
         self._image_counters = {}
 
         self._ressetable_send_delay_ms = 0 # Delay time in milliseconds before sending a serial command after receiving idle/CTS.
+
+    @property
+    def _is_machine_busy(self):
+        return self._working_thread is not None or self._is_machine_paused
+
+    @property
+    def _machine_last_reported_on(self):
+        reports = [dvc.last_reported_on for dvc in self.project.devices if dvc.last_reported_on]
+        return max(reports) if len(reports) > 0 else None
+
+    @property
+    def _is_machine_locked(self):
+        for dvc in self.project.devices:
+            if dvc.serial_response:
+                flags = dvc.serial_response.system_status_flags
+                if flags and len(flags) == 8 and int(flags[0]):
+                    return True
+
+        return False
+
+    @property
+    def _has_machine_reported(self):
+        if any(not dvc.serial_response for dvc in self.project.devices):
+            return False
+
+        return all(dvc.serial_status != ComStatus.UNKNOWN for dvc in self.project.devices)
+
+    @property
+    def _disengage_motors_commands(self):
+        cmds = []
+        for dvc in self.project.devices:
+            cmds.append(Action(ActionType.M18, dvc.device_id))
+
+        actions = []
+        actions.append(cmds)
+        return actions
+
+    @property
+    def machine_status(self):
+        """Returns the machine's status."""
+        status = 'unknown'
+
+        if self._is_machine_paused:
+            status = 'paused'
+        else:
+            statuses = list(set(dvc.status for dvc in self.project.devices))
+
+            if len(statuses) == 1 and statuses[0]:
+                status = statuses[0].name.lower()
+            elif len(statuses) > 1:
+                status = 'mixed'
+
+        return status
+
+    @property
+    def is_machine_idle(self):
+        """Returns a value indicating whether the machine is idle."""
+        return all(dvc.status == ComStatus.IDLE for dvc in self.project.devices)
+
+    @property
+    def is_machine_homed(self):
+        """Returns a value indicating whether the machine is homed."""
+        return all(dvc.is_homed for dvc in self.project.devices)
 
     @property
     def serial_bauds(self):
@@ -216,6 +277,102 @@ class COPISCore(
     def is_dev_env(self):
         """Returns a flag indicating whether we are in a dev environment."""
         return self._is_dev_env
+
+    def _get_move_commands(self, is_absolute, *device_ids):
+        actions = []
+        all_device_ids = [dvc.device_id for dvc in self.project.devices]
+
+        if device_ids and all(did in all_device_ids for did in device_ids):
+            atype = ActionType.G90 if is_absolute else ActionType.G91
+            cmds = []
+
+            for did in device_ids:
+                cmds.append(Action(atype, did))
+
+            actions.append(cmds)
+
+        return actions
+
+    def _query_machine(self):
+        print_debug_msg(self.console, '**** Querying machine ****', self._is_dev_env)
+        cmds = []
+
+        if self._is_machine_busy:
+            print_error_msg(self.console, 'Cannot query. The machine is busy.')
+            return
+
+        cmds.append(Action(ActionType.M120, 0))
+
+        if cmds:
+            self._keep_working = True
+            self._clear_to_send = True
+            self._mainqueue = []
+            self._mainqueue.append(cmds)
+            self._send_next()
+            self._keep_working = False
+
+    def _unlock_machine(self):
+        print_debug_msg(self.console, '**** Unlocking machine ****', self._is_dev_env)
+        cmds = []
+
+        if self._is_machine_busy:
+            print_error_msg(self.console, 'Cannot unlock. The machine is busy.')
+            return False
+
+        for dvc in self.project.devices:
+            if dvc.serial_response:
+                flags = dvc.serial_response.system_status_flags
+
+                if flags and len(flags) == 8 and int(flags[0]):
+                    cmd = Action(ActionType.M511, dvc.device_id)
+                    cmds.append(cmd)
+
+        if cmds:
+            self._keep_working = True
+            self._clear_to_send = True
+            self._mainqueue = []
+            self._mainqueue.append(cmds)
+            self._send_next()
+            self._keep_working = False
+            return True
+
+        return False
+
+    def _get_initialization_commands(self, atype: ActionType):
+        step_1 = []
+        step_2 = []
+        actions = []
+        g_code = str(atype).split('.')[1]
+
+        for dvc in self.project.devices:
+            device_id = dvc.device_id
+            cmd_id = ''
+            cmd_str_1 = ''
+            cmd_str_2 = ''
+            x, y, z, p, t = self._get_device(device_id).home_position
+
+            if device_id > 0:
+                cmd_id = f'>{device_id}'
+
+            cmd_str_1 = f'{cmd_id}{g_code}Z{z}'
+            cmd_str_2 = f'{cmd_id}{g_code}X{x}Y{y}P{p}T{t}'
+
+            step_1.append(deserialize_command(cmd_str_1))
+            step_2.append(deserialize_command(cmd_str_2))
+
+        actions.append(step_1)
+        actions.append(step_2)
+        return actions
+
+    def _reconcile_machine(self, dvc_statuses):
+        for status in dvc_statuses:
+            if status:
+                did, is_homed, resp = status
+                device = self._get_device(did)
+
+                if is_homed and device and resp:
+                    device.set_serial_response(resp)
+                    device.set_is_homed()
 
     def _get_active_serial_port_name(self):
         port = next(
@@ -609,6 +766,119 @@ class COPISCore(
         dispatcher.disconnect(self._on_device_ser_updated, signal='ntf_device_ser_updated')
         dispatcher.disconnect(self._on_device_eds_updated, signal='ntf_device_eds_updated')
 
+    def set_ready(self):
+        """Initializes the gantries to their current positions."""
+        def set_ready_callback():
+            for dvc in self.project.devices:
+                dvc.set_is_homed()
+
+        if self._is_machine_busy:
+            print_error_msg(self.console, 'Cannot set or go to ready. The machine is busy.')
+            return
+
+        init_code = ActionType.G1 if self.is_machine_homed else ActionType.G92
+        cmds = self._get_initialization_commands(init_code)
+
+        if cmds:
+            self._mainqueue = []
+            self._mainqueue.extend(cmds)
+            self._mainqueue.extend(self._disengage_motors_commands)
+            self._work_type = WorkType.SET_READY
+
+            self._keep_working = True
+            self._clear_to_send = True
+            self._working_thread = threading.Thread(
+                target=self._worker,
+                name='working thread',
+                kwargs={
+                    "extra_callback": set_ready_callback
+                }
+            )
+            self._working_thread.start()
+
+    def jog(self, action: Action):
+        """Jogs the machine according to the provided action."""
+        if not self.is_serial_port_connected:
+            print_error_msg(self.console,
+                'The machine needs to be connected before jogging can start.')
+            return
+
+        if self._is_machine_busy:
+            print_error_msg(self.console, 'Cannot jog. The machine is busy.')
+            return
+
+        if not self.is_machine_idle:
+            print_error_msg(self.console, 'The machine needs to be homed before jogging can start.')
+            return
+
+        header = self._get_move_commands(False, action.device)
+        body = [action]
+        footer = self._get_move_commands(True, action.device)
+
+        self._mainqueue = []
+        self._mainqueue.extend(header)
+        self._mainqueue.append(body)
+        self._mainqueue.extend(footer)
+        self._work_type = WorkType.JOGGING
+
+        self._keep_working = True
+        self._clear_to_send = True
+        self._working_thread = threading.Thread(
+            target=self._worker,
+            name='working thread'
+        )
+        self._working_thread.start()
+
+    def play_poses(self, poses: List[Pose]):
+        """Play the given pose set."""
+        self._session_guid = str(uuid.uuid4())
+        process_poses = lambda: [[val for val in tup if val is not None] for tup in
+            zip_longest(*[p.get_seq_actions() for p in poses])]
+
+        processed_poses = process_poses()
+        commands = []
+        commands.extend([c for p in processed_poses for c in p])
+        is_serial_needed = any(get_atype_kind(p.atype) == 'SER' for p in commands)
+        is_edsdk_needed = any(get_atype_kind(p.atype) == 'EDS' for p in commands)
+
+        if is_serial_needed:
+            if not self.is_serial_port_connected:
+                print_error_msg(self.console,
+                    'The machine needs to be connected before stepping can start.')
+                return
+
+            if self._is_machine_busy:
+                print_error_msg(self.console, 'Cannot step. The machine is busy.')
+                return
+
+            if not self.is_machine_idle:
+                print_error_msg(self.console,
+                    'The machine needs to be homed before stepping can start.')
+                return
+
+        if is_edsdk_needed:
+            if not self._is_edsdk_enabled:
+                print_error_msg(self.console, 'EDSDK is not enabled.')
+                return
+
+        dispatcher.connect(self._on_device_ser_updated, signal='ntf_device_ser_updated')
+        dispatcher.connect(self._on_device_eds_updated, signal='ntf_device_eds_updated')
+
+        self._mainqueue = []
+        self._mainqueue.extend(processed_poses)
+        self._work_type = WorkType.IMAGING  
+
+        self._keep_working = True
+        self._clear_to_send = True
+        self._working_thread = threading.Thread(
+            target=self._worker,
+            name='working thread',
+            kwargs={
+                "extra_callback": self._imaging_callback
+            }
+        )
+        self._working_thread.start()
+
     def init_serial(self) -> None:
         """Initializes the serial controller."""
         if self._is_serial_enabled:
@@ -747,7 +1017,6 @@ class COPISCore(
             device = self._get_device(device_id)
 
             if device:
-                #connected = self._edsdk.connect(device.port, device_id)
                 connected = self._edsdk.connect(device)
             else:
                 print_error_msg(self.console, f'Camera {device_id} cannot be found.')
