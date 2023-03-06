49e9cd0b2c2a68e16bb04ab487e63dc6c0f478d2
djihbril <djihbril@outlook.com>
2023-03-06 11:41:55 -0500

    Cleaning up the file

--- copis/classes/sys_db.py
+++ copis/classes/sys_db.py
@@ -1,25 +1,41 @@
+# This file is part of COPISClient.
+#
+# COPISClient is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# COPISClient is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with COPISClient. If not, see <https://www.gnu.org/licenses/>.
+
+"""SQLite database management object."""
 
 import time
 import sqlite3
-from sqlite3 import Error
 from copis import store
 from copis.classes import Device
 from copis.helpers import hash_file_md5
+
 class SysDB():
-    """Represents a system database object in sqlite that can be used for tracking all image and serial requests. 
-       Sys db can be used to sync images with positional information 
+    """Represents a system database object in sqlite that can be used for tracking all image and serial requests.
+       Sys db can be used to sync images with positional information
     """
 
-    _schema = {'image_metadata': {'id': 'INTEGER PRIMARY KEY AUTOINCREMENT', 
-                   'x': 'REAL', 'y': 'REAL','z': 'REAL','p': 'REAL','t': 'REAL',
-                   'src' : 'TEXT', 'img1_md5' : 'TEXT', 'img1_fname' : 'TEXT', 'img2_md5': 'TEXT',  'img2_fname' : 'TEXT', 'session_id' : 'INTEGER',
-                   'cam_id' : 'INTEGER', 'cam_serial_no': 'TXT', 'cam_name' : 'TEXT', 'cam_type' : 'TEXT', 'cam_desc' : 'TEXT', 
-                   'unix_time_start' : 'REAL', 'unix_time_end' : 'REAL',
-                   },
-               'serial_tx': {'id' : 'INTEGER PRIMARY KEY AUTOINCREMENT',
-                             'data' : 'BLOB', 'unix_time' : 'REAL'},
-               'serial_rx': {'id' : 'INTEGER PRIMARY KEY AUTOINCREMENT',
-                             'data' : 'BLOB', 'unix_time' : 'REAL'}
+    _schema = {'image_metadata': {'id': 'INTEGER PRIMARY KEY AUTOINCREMENT',
+                                  'x': 'REAL', 'y': 'REAL', 'z': 'REAL', 'p': 'REAL', 't': 'REAL',
+                                  'src': 'TEXT', 'img1_md5': 'TEXT', 'img1_fname': 'TEXT', 'img2_md5': 'TEXT', 'img2_fname': 'TEXT', 'session_id': 'INTEGER',
+                                  'cam_id': 'INTEGER', 'cam_serial_no': 'TXT', 'cam_name': 'TEXT', 'cam_type': 'TEXT', 'cam_desc': 'TEXT',
+                                  'unix_time_start': 'REAL', 'unix_time_end': 'REAL',
+                                  },
+               'serial_tx': {'id': 'INTEGER PRIMARY KEY AUTOINCREMENT',
+                             'data': 'BLOB', 'unix_time': 'REAL'},
+               'serial_rx': {'id': 'INTEGER PRIMARY KEY AUTOINCREMENT',
+                             'data': 'BLOB', 'unix_time': 'REAL'}
                }
 
     def __init__(self):
@@ -28,22 +44,23 @@ class SysDB():
             self._is_initialized = False
             return
         self._create_or_update_schema()
-        self._poses_in_play = {} #dict keeping containing devices id's and corresponding database id's for poses "in play" (ie awaiting idle respose after imaging)
+
+        # Dict keeping containing devices id's and corresponding database id's for poses "in play" (ie awaiting idle response after imaging)
+        self._poses_in_play = {}
+
         self._is_initialized = True
         print("using sysdb: ", self._filename )
-    
 
     @property
     def is_initialized(self) -> bool:
-        """Returns True if db has been successfully initialized, false otherwise """
+        """Returns True if db has been successfully initialized, false otherwise."""
         return self._is_initialized
 
     def _create_or_update_schema(self):
-        """Creates databse or checks to make sure schema is up to date, 
-        adding missings columns to tables """
+        """Creates database or checks to make sure schema is up to date, adding missing columns to tables."""
         db =  sqlite3.connect(self._filename)
         for tbl_name, tbl_schema in self._schema.items():
-            s = f'SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'{tbl_name}\'';
+            s = f'SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'{tbl_name}\''
             if len(db.execute(s).fetchall()) == 0:
                 fields = ','.join(' '.join((k,v)) for (k,v) in tbl_schema.items())
                 s = f'CREATE TABLE IF NOT EXISTS {tbl_name} ({fields});'
@@ -51,15 +68,16 @@ class SysDB():
             else:
                 s = f'PRAGMA table_info({tbl_name})'
                 rows = db.execute(s,[]).fetchall()
-                for k,v in tbl_schema.items():    
+                for k, v in tbl_schema.items():
                     if len ([item for item in rows if item[1] == k]) == 0:
-                        s = f'ALTER TABLE {tbl_name} ADD COLUMN {k} {v};';
+                        s = f'ALTER TABLE {tbl_name} ADD COLUMN {k} {v};'
                         print(s)
                         db.execute(s)
         db.commit()
         db.close()
-    
+
     def last_session_id(self) -> int:
+        """Returns the last imaging session id."""
         if not self._is_initialized:
             return -1
         result = -1
@@ -75,39 +93,46 @@ class SysDB():
         return result
 
     def serial_tx(self, b : bytes) -> int:
+        """Inserts a command in the transmit table and returns the last record's id."""
         if not self._is_initialized:
             return -1
+
         s = 'INSERT INTO serial_tx (data, unix_time) VALUES(?,?);'
         db = sqlite3.connect(self._filename)
         cur = db.cursor()
-        v = (b,time.time() )
+        v = (b,time.time())
         cur.execute(s, v)
-        id = cur.lastrowid
+        row_id = cur.lastrowid
         cur.close()
         db.commit()
         db.close()
-        return id
+        return row_id
 
     def serial_rx(self, b : bytes) -> int:
+        """Inserts a controller response in the receive table and returns the last record's id."""
         if not self._is_initialized:
             return -1
+
         if len(b) <1:
             return -2
+
         s = 'INSERT INTO serial_rx (data, unix_time) VALUES(?,?);'
         db = sqlite3.connect(self._filename)
         cur = db.cursor()
         v = (b,time.time() )
         cur.execute(s, v)
-        id = cur.lastrowid
+        row_id = cur.lastrowid
         cur.close()
         db.commit()
         db.close()
-        return id
+        return row_id
 
     def start_pose(self, device: Device, src: str, session_id=-1) -> int:
+        """Records the start of a picture taking action and returns the last record's id."""
         if not self._is_initialized:
             return -1
-        id = -1
+
+        row_id = -1
         v = (session_id,      \
              device.position.x, \
              device.position.y, \
@@ -126,21 +151,28 @@ class SysDB():
         db = sqlite3.connect(self._filename)
         cur = db.cursor()
         cur.execute(s, v)
-        id = cur.lastrowid
+        row_id = cur.lastrowid
+
         if device.device_id not in self._poses_in_play:
-            self._poses_in_play[device.device_id] = [id]
+            self._poses_in_play[device.device_id] = [row_id]
         else:
-            self._poses_in_play[device.device_id].append(id)
+            self._poses_in_play[device.device_id].append(row_id)
+
         cur.close()
         db.commit()
         db.close()
-        return id
+        return row_id
 
     def update_pose_output(self, device: Device, fname:str) -> int:
+        """Updates a picture taking action entry and returns the entry's id."""
         if not self._is_initialized:
             return -1
+
         if device.device_id in self._poses_in_play and len(self._poses_in_play[device.device_id]) > 0:
-            p_id = self._poses_in_play[device.device_id][0] #the reason we maintain a list even thoug only one image can be taken at a time per device, is that in case the device signals it is finished and recieves another image request while we are still processing the last.
+            # The reason we maintain a list even though only one image can be taken at a time per device, is that in case the device signals it is finished and
+            # receives another image request while we are still processing the last.
+            p_id = self._poses_in_play[device.device_id][0]
+
             db = sqlite3.connect(self._filename)
             cur = db.cursor()
             s = f'select img1_fname, img2_fname from image_metadata WHERE id = {p_id} and unix_time_end is null;'
@@ -166,10 +198,15 @@ class SysDB():
         return -2
 
     def end_pose(self, device: Device) -> int:
+        """Records the end of a picture taking action and returns the entry's id."""
         if not self._is_initialized:
             return -1
         if device.device_id in self._poses_in_play and len(self._poses_in_play[device.device_id]) > 0:
-            p_id = self._poses_in_play[device.device_id][0] #the reason we maintain a list even thoug only one image can be taken at a time per device, is that in case the device signals it is finished and recieves another image request while we are still processing the last.
+
+            # The reason we maintain a list even though only one image can be taken at a time per device, is that in case the device signals it is finished and
+            # receives another image request while we are still processing the last.
+            p_id = self._poses_in_play[device.device_id][0]
+
             self._poses_in_play[device.device_id].pop(0)
             db = sqlite3.connect(self._filename)
             cur = db.cursor()
@@ -181,9 +218,3 @@ class SysDB():
             db.close()
             return p_id
         return -2
-
-
-
-    
-
-    
