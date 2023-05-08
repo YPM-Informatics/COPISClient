e67ebb7fbda3202375c9e4b3ee0409e29f5c5101
Djihbrihou Abibou <djihbril@outlook.com>
2023-05-08 07:37:37 -0400

    Refactoring pathutils module structure

--- copis/utils/path.py
+++ copis/utils/path.py
@@ -15,7 +15,10 @@
 
 """Imaging path management functions."""
 
+import operator as op
+
 from collections import defaultdict
+from itertools import groupby
 from typing import List
 
 from copis.helpers import get_heading, interleave_lists, sanitize_number, sanitize_point
@@ -33,31 +36,42 @@ def build_path(devices: dict, vertices: dict, target: Point3) -> List[List[Move]
     """
     keyed_moves = defaultdict(list)
 
-    # Fill all extend all arrays with their last items, up to the longest arrays length.
+    # Extend all arrays with their last items, up to the longest array's length.
     # We do this to ensure each move set has a move from each device, even if a device
     # does not move in a particular set.
     max_len = max(map(len, vertices.values()))
 
-    for l in vertices.values():
-        cur_len = len(l)
-        if cur_len < max_len:
-            l.extend([l[-1]] * (max_len - cur_len))
+    for col in vertices.values():
+        if len(col) < max_len:
+            col.extend([col[-1]] * (max_len - len(col)))
 
-    for dvc_id, dvc in devices.items():
+    for dvc_id in devices:
         last_pose = None
 
         for vtx in vertices[dvc_id]:
             pan, tilt = get_heading(vtx, target)
-
-            position = Point5(*sanitize_point(vtx), sanitize_number(pan), sanitize_number(tilt))
             start_pose = last_pose
-            end_pose = Pose(position, [ShutterReleaseAction(1500)])
+            end_pose = Pose(Point5(*sanitize_point(vtx), sanitize_number(pan), sanitize_number(tilt)), [ShutterReleaseAction(1500)])
 
-            keyed_moves[dvc_id].append(Move(MoveTypes.LINEAR, start_pose, end_pose, device=dvc))
+            keyed_moves[dvc_id].append(Move(MoveTypes.LINEAR, start_pose, end_pose, device=devices[dvc_id]))
 
             last_pose = end_pose
 
     interleaved = interleave_lists(*list(keyed_moves.values()), keep_def_fill_val=True)
-    set_size = len(devices)
+    lists = [interleaved[i:i+len(devices)] for i in range(0, len(interleaved), len(devices))]
+
+    return [[m for m in l if m is not None] for l in lists]
+
+
+def interleave_moves(moves: List[Move]) -> List[List[Move]]:
+    """Returns an interleaved COPIS imaging path as a list of lists of moves.
+        Args:
+            moves: a collection of moves.
+    """
+    keyed_moves = groupby(sorted(moves, key=op.attrgetter('device.d_id')), op.attrgetter('device.d_id'))
+    groups = [list(g) for _, g in keyed_moves]
+    interleaved = interleave_lists(*groups, keep_def_fill_val=True)
+    set_size = len(groups)
     lists = [interleaved[i:i+set_size] for i in range(0, len(interleaved), set_size)]
+
     return [[m for m in l if m is not None] for l in lists]
